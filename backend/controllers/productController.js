1d973da5e676ab5379235857b6366c43ec6f4470
DomLeng <34336728@qq.com>
2025-10-06 12:37:11 +0100

    fixed the search problems on the first page

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,18 +6,21 @@ import Product from '../models/productModel.js'
 // @access  public
 
 const getProducts = asyncHandler(async (req,res) => {
-    const pageSize = process.env.PAGINATION_LIMIT || 8
+    const pageSize = Number(process.env.PAGINATION_LIMIT) || 8
     const page = Number(req.query.pageNumber) || 1
+
+    console.log(req.query.keyword);
 
     const keyword = req.query.keyword ? 
         {
             name:{
                 $regex:req.query.keyword,
-                $option:'i'
+                $options:'i'
             }
         }:{}
     
     const count = await Product.countDocuments({...keyword})
+    console.log(count);
     const products = await Product.find({...keyword}).limit(pageSize).skip(pageSize * (page - 1))
 
     res.json({products,page,pages: Math.ceil(count / pageSize)})
